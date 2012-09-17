928c95c82a77554eb56ef1f09d1f0ec32122bc31
DbIHbKA <dbihbka@xakep.ru>
2012-09-17 12:59:02 +0400

    Add folder pure_python

---
+++ practice/python/pure_python/3-sum.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function
+
+
+def read(filename):
+    with open(filename) as f:
+        n = int(f.readline())
+        data = list(map(int, f.readline().split(' ')))
+    return n, data
+
+
+def bin_search(a, x):
+    first = 0
+    last = len(a)
+    while first < last:
+        mid = first + (last - first) // 2
+        if x <= a[mid]:
+            last = mid
+        else:
+            first = mid + 1
+    if last < len(a) and a[last] == x:
+        return True
+    else:
+        return False
+
+
+def main(filename):
+    n, data = read(filename)
+    data.sort()
+    count = 0
+    for i in range(n - 2):
+        for j in range(i + 1, n - 1):
+            if bin_search(data[j + 1:], -(data[i] + data[j])):
+                count += 1
+    return count
+
+
+if __name__ == '__main__':
+    import sys
+    print(main(sys.argv[1]))
