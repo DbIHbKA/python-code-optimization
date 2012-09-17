48c472277b1986557309e942190452af489fc87c
DbIHbKA <dbihbka@xakep.ru>
2012-09-17 11:25:15 +0400

    Add tester.py. That code generate 3 files with test data

--- practice/tests/tester.py
+++ practice/tests/tester.py
@@ -1,2 +1,17 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
+from random import randint
+
+
+def main():
+    for num in [8000, 16000, 32000, 64000]:
+        filename = "{0}Kint.txt".format(num / 1000)
+        with open(filename, "w") as f:
+            f.writeline(num)
+            for x in range(num):
+                f.write(randint(-100000, 100000))
+
+
+if __name__ == '__main__':
+    main()
