fb588540e058c36b06c7a6369f3335c40cc46218
DbIHbKA <dbihbka@xakep.ru>
2012-09-17 11:46:53 +0400

    Generate some tests

--- practice/tests/tester.py
+++ practice/tests/tester.py
@@ -5,12 +5,14 @@ from random import randint
 
 
 def main():
-    for num in [8000, 16000, 32000, 64000]:
-        filename = "{0}Kint.txt".format(num / 1000)
+    for num in [1000, 2000, 4000, 8000, 16000, 32000, 64000]:
+        filename = "{0}Kint.txt".format(num // 1000)
         with open(filename, "w") as f:
-            f.writeline(num)
-            for x in range(num):
-                f.write(randint(-100000, 100000))
+            f.write(str(num) + '\n')
+            temp = set()
+            while len(temp) < num:
+                temp.add(str(randint(-100000, 100000)))
+            f.write(' '.join(list(temp)))
 
 
 if __name__ == '__main__':
